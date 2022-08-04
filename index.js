c3e853605544ce0b8c14e218138751ae3879e731
Elvis <s49188705@gmail.com>
2022-08-04 21:40:14 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+    return 1 + 80;
+}
+function subtract (){
+    return 60-40;
+}
+
+function multiply(){
+    return 3.4 * 2;
+}
+
+function divide (){
+    return 100/20;
+}
+
+function add (a, b){
+    return a+b;
+}
+
+function subtract (a, b){
+    return a-b;
+}
+
+function multiply (a, b){
+    return a * b;
+}
+
+function divide (a, b){
+    return a / b ;
+}
+
+function increment(n){
+    n++;
+    return(n);
+}
+ function decrement(n){
+    n--;
+    return(n);
+ }
+
+ function makeInt(n){
+    return parseInt(n, 10);
+ }
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+
